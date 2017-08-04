7a5f9a5ea4a27a7abeaa8c7f1ff8411cd70c0b85
Lorand Gombos <lorand.gombos91@gmail.com>
2017-08-04 16:12:06 +0300

    file upload finished on frontend

--- DotKernel/frontend/Sound.php
+++ DotKernel/frontend/Sound.php
@@ -6,4 +6,9 @@ class Sound extends Dot_Model
     {
         parent::__construct();
     }
+    #inserts a new upload into table sound
+    public function insertUpload($valueArray)
+    {
+        $insert = $this->db->insert('sound', $valueArray);
+    }
 }
